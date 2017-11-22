dd7e581cc27862a277b4e9445338ee01bbb18021
Aken Mehta <mehta.aken@gmail.com>
2017-11-22 00:36:38 -0500

    Make site responsive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
+var app = {};
+app.windowSize = $(window).width();
 
+app.toggleClassInvisible = function() {
+	if(app.windowSize <=668) {
+		$('.header__item:last-child').removeClass('hide-me');
+	}
+	else{
+		$('.header__item:last-child').addClass('hide-me');
+	}
+};
+
+app.windowResize = function() {
+	$(window).resize(function() {
+  		app.windowSize = $(window).width();
+  		app.toggleClassInvisible();
+  		// app.showNavBtn();
+		console.log(app.windowSize);
+	});
+};
+
+app.init = function() {
+	app.windowResize();
+	app.toggleClassInvisible();
+};
+
+$(function() {
+	app.init();
+});
